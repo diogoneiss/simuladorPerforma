cb1be597b90b26476d81eac59584ca377660c71d
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2019-09-29 11:46:27 -0300

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,68 +1,216 @@
 //funcoes para pegar os valores dos elementos de cada questão do questionário. Haverá também um alert caso a pessoa não selecione algum campo
 
 
-	 function Simulacao() {
-			
-			var form = document.forms("formSimulador");
-			var faturamento = 0;
-			//var respostas = getSelectedValues();
-			var dinheiroPerdido = 0;
 
-			dinheiroPerdido += calcularMaquinas();
-
-			alert(dinheiroPerdido);
-			return dinheiroPerdido;
-		}
-
-		function calcularMaquinas(){
-			var maquinasValores = new Array();
-			var form = document.forms("formSimulador");
-			var faturamento = form.elements["faturamento"];
-			
-			maquinasValores[1] = 0.1 / 100;
-			maquinasValores[2] = 0.2 / 100;
-			maquinasValores[3] = 0.3 / 100;
-			maquinasValores[4] = 0.4 / 100;
-			maquinasValores[5] = 0.5 / 100;
-
-			var maquinaSelecionada = form.elements["maquinas"];
-			var dinheiroMaq = maquinasValores[maquinaSelecionada];
-			alert("Dinheiro perdido com máquina: "+dinheiroMaq);
-			return faturamento * dinheiroMaq;
-		}
-
-		
-		
+var maquinasValores = [];
+maquinasValores[1] = 0.02 / 100;
+maquinasValores[2] = 0.03 / 100;
+maquinasValores[3] = 0.05 / 100;
+maquinasValores[4] = 0.07/ 100;
 
 
-    function getSelectedValues(){
+var adquirintesValores = [];
+adquirintesValores[1] = 0.02 / 100;
+adquirintesValores[2] = 0.03 / 100;
+adquirintesValores[3] = 0.04 / 100;
+adquirintesValores[4] = 0.05 / 100;
 
-            var selectedFaturamento = document.getElementById("faturamento").value;
-            var selectedAdquirintes= document.getElementById("adquirintes").value;
-            var selectedMaquinas= document.getElementById("maquinas").value;
-            var selectedConferenciaVendas = document.getElementById("conferenciaVendas").value;
-            /*
-            if(selectedFaturamento == 0 || selectedMaquinas == 0 || selectedConferenciaVendas == 0 || selectedAdquirintes == 0)
-              alert("erro! Algum dos valores não foi selecionado.");
-            else
-              alert(selectedFaturamento + selectedAdquirintes + selectedMaquinas + selectedConferenciaVendas);
-				*/
-				var retorno = {
-					fat: selectedFaturamento,
-					adq: selectedAdquirintes,
-					maq: selectedMaquinas,
-					conf: selectedConferenciaVendas
-				};
-				alert(retorno.fat);
-				return retorno;
-		  }
 
-	function escreverPerda(){
-			var perda = Simulacao();
-			alert("Perda: "+perda);
-			//alert("Cheguei na funcao de escrever perda");
-			document.getElementsById("textoPlaceholder").value =  "Você está perdendo "+perda + " reais.";
 
-			alert("Você está perdendo "+perda + " reais.");
-		}
+var cartaoBeneficioValores = [];
+cartaoBeneficioValores[1] = 0.25 / 100;
+cartaoBeneficioValores[2] = 0;
 
+
+
+var conferenciaVendasValores = [];
+conferenciaVendasValores[1] = 0.09 / 100;
+conferenciaVendasValores[2] = 0.07 / 100;
+conferenciaVendasValores[3] = 0.05 / 100;
+conferenciaVendasValores[4] = 0.03 / 100;
+
+
+var antecipacaoValores = [];
+antecipacaoValores[1] = 0.5 / 100;
+antecipacaoValores[2] = 0.85 / 100;
+antecipacaoValores[3] = 1 / 100;
+
+var cobrancaAlugueisValores = [];
+cobrancaAlugueisValores[1] = 0;
+cobrancaAlugueisValores[2] = 0.1/100;
+
+// funcao principal do codigo
+function Simulacao() {
+	////console.log("Abri a funcao de simulacao de funcoes corretamente.");
+
+	var dinheiroPerdido = 0;
+	dinheiroPerdido += calcularMaquinas();
+	dinheiroPerdido += calcularConferenciaVendas();
+	dinheiroPerdido += calcularAdquirintes();
+	dinheiroPerdido += calcularAntecipacaoValores();
+	dinheiroPerdido += calcularCartaoBeneficio();
+	dinheiroPerdido += calcularAluguelMaquinas();
+
+
+
+	
+	return dinheiroPerdido;
+}
+
+//calcular quanto dinheiro foi perdido com a maquina selecionada em específico.
+function calcularMaquinas() {
+
+	var faturamento = calcularFaturamento();
+	//console.log("Faturamento: " + faturamento);
+	var form = document.forms.formSimulador;
+
+	var maquinaSelecionada = form.elements.maquinas.value;
+	//console.log("Maquina selecionada: " + maquinaSelecionada);
+
+	var dinheiroMaq = maquinasValores[maquinaSelecionada];
+
+	dinheiroMaq *= faturamento;
+	//console.log("Dinheiro perdido aqui: "+dinheiroMaq);
+	return dinheiroMaq;
+}
+
+function calcularAdquirintes() {
+
+	var faturamento = calcularFaturamento();
+
+	var form = document.forms.formSimulador;
+
+	var adquirinteSelecionada = form.elements.adquirintes.value;
+	//console.log("Adquirintes selecionadas: " + adquirinteSelecionada);
+
+	var dinheiroMaq = adquirintesValores[adquirinteSelecionada];
+
+	dinheiroMaq *= faturamento;
+	//console.log("Dinheiro perdido aqui: "+dinheiroMaq);
+	return dinheiroMaq;
+}
+
+//funcao precisa de radios pra funcionar
+function calcularCartaoBeneficio() {
+
+	var faturamento = calcularFaturamento();
+	var cartaoBeneficio;
+
+	var form = document.forms.formSimulador;
+
+	if(form.elements.cartaobeneficio0.checked == true){
+		cartaoBeneficio = form.elements.cartaobeneficio0.value;
+		//console.log("Cartao beneficio selecionado: " + cartaoBeneficio);
+	}
+	else{
+		cartaoBeneficio = form.elements.cartaobeneficio1.value;
+		//console.log("Cartao beneficio selecionado: " + cartaoBeneficio);
+	}
+
+	var dinheiroMaq = cartaoBeneficioValores[cartaoBeneficio];
+
+	dinheiroMaq *= faturamento;
+	//console.log("Dinheiro perdido aqui: "+dinheiroMaq);
+	return dinheiroMaq;
+}
+
+function calcularAluguelMaquinas() {
+
+	var faturamento = calcularFaturamento();
+	var aluguelMaquinas;
+
+	var form = document.forms.formSimulador;
+
+	if(form.elements.aluguelMaquinas0.checked == true){
+		aluguelMaquinas = form.elements.aluguelMaquinas0.value;
+		//console.log("AluguelMaquinas selecionado: " + aluguelMaquinas);
+	}
+	else{
+		aluguelMaquinas = form.elements.aluguelMaquinas1.value;
+		//console.log("AluguelMaquinas selecionado: " + aluguelMaquinas);
+	}
+
+	var dinheiroMaq = cobrancaAlugueisValores[aluguelMaquinas];
+
+	dinheiroMaq *= faturamento;
+	//console.log("Dinheiro perdido aqui: "+dinheiroMaq);
+	return dinheiroMaq;
+}
+
+function calcularConferenciaVendas() {
+
+	var faturamento = calcularFaturamento();
+
+	var form = document.forms.formSimulador;
+
+	var conferenciaSelecionada = form.elements.conferenciaVendas.value;
+	//console.log("ConferenciaVendas selecionada: " + conferenciaSelecionada);
+
+	var dinheiroMaq = conferenciaVendasValores[conferenciaSelecionada];
+
+	dinheiroMaq *= faturamento;
+	//console.log("Dinheiro perdido aqui: "+dinheiroMaq);
+	return dinheiroMaq;
+}
+
+function calcularAntecipacaoValores() {
+
+	var faturamento = calcularFaturamento();
+
+	var form = document.forms.formSimulador;
+
+	var antecipacaoSelecionada = form.elements.antecipacao.value;
+	//console.log("antecipacaoSelecionada selecionada: " + antecipacaoSelecionada);
+
+	var dinheiroMaq = antecipacaoValores[antecipacaoSelecionada];
+
+	dinheiroMaq *= faturamento;
+	//console.log("Dinheiro perdido aqui: "+dinheiroMaq);
+	return dinheiroMaq;
+}
+
+function calcularFaturamento() {
+	var form = document.forms.formSimulador;
+	var faturamento = form.elements.faturamento.value;
+
+	////console.log("Faturamento na funcao: " + faturamento);
+	return faturamento;
+}
+
+//verificar se todas os campos foram preenchidos
+function tudoPreenchido(){
+	var tudoCertinho = true;
+	var form = document.forms.formSimulador;
+
+	if(form.elements.antecipacao.value == 0)
+		tudoCertinho = false;
+	if( form.elements.faturamento.value == 0)
+		tudoCertinho = false;
+	if(form.elements.conferenciaVendas.value == 0)
+		tudoCertinho = false;
+	if(form.elements.adquirintes.value == 0)
+		tudoCertinho = false;
+	if(form.elements.maquinas.value == 0)
+		tudoCertinho = false;
+	if(form.elements.cartaobeneficio0.checked == false && form.elements.cartaobeneficio1.checked == false)
+		tudoCertinho = false;
+	if(form.elements.aluguelMaquinas0.checked == false && form.elements.aluguelMaquinas1.checked == false)
+		tudoCertinho = false;	
+	
+	return tudoCertinho;	
+}
+
+function escreverPerda() {
+	// so vai executar se tudo estiver preenchido corretamente.
+	if(tudoPreenchido()){
+		var perda = Simulacao();
+		//arredondar
+		perda = Math.round(perda);
+		//alert("Perda: "+perda);
+		//alert("Cheguei na funcao de escrever perda");
+		document.getElementById("outputSimulador").innerHTML = "Você pode estar perdendo aproximadamente R$ " + perda + ",00 	";
+		document.getElementById("botaoPreencher").style = "display: inline-block";
+	}
+	else
+		document.getElementById("outputSimulador").innerHTML = "Você não respondeu todos os campos necessários..";
+}
