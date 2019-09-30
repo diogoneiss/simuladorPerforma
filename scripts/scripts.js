3b6996b8ac716cee524df06beb5f88c42f910c92
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2019-09-29 21:15:17 -0300

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -212,35 +212,3 @@ function escreverPerda() {
 		document.getElementById("outputSimulador").innerHTML = "Você não respondeu todos os campos necessários..";
 }
 
-function validarForm() {
-	document.getElementById('status').innerHTML = "Sending...";
-
-    formData = {
-        'name'     : $('input[name=name]').val(),
-        'email'    : $('input[name=email]').val(),
-        'Telefone'  : $('textarea[name=Telefone]').val()
-    };
-
-
-   $.ajax({
-    url : "mailResposta.php",
-    type: "POST",
-    data : formData,
-    success: function(data, textStatus, jqXHR)
-    {
-
-        $('#status').text(data.message);
-        if (data.code){//If mail was sent successfully, reset the form.
-        	$('#contact-form').closest('form').find("input[type=text], textarea").val("");
-			document.getElementById(simuladorCompleto).style = "visibility: visible;";
-		}
-		},
-	
-    error: function (jqXHR, textStatus, errorThrown)
-    {
-        $('#status').text(jqXHR);
-    }
-});
-
-
-}
